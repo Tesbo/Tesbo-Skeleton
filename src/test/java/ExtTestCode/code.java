4cb3266c764381ca8a211dd5694f5ba56d3545b3
viralpatel <vir@qable.io>
2019-04-23 23:02:31 +0530

    Updated the Skeleton Project based on the New Tesbo Version

--- src/test/java/ExtTestCode/code.java
+++ src/test/java/ExtTestCode/code.java
@@ -2,19 +2,17 @@ package ExtTestCode;
 
 
 import ExtCode.ExtCode;
-import Runner.Runner;
 import Selenium.ExtendTesboDriver;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.testng.Assert;
+
 
 
 public class code extends ExtendTesboDriver{
     public code(WebDriver driver) {
         super(driver);
     }
-
-
 
     @ExtCode("Search books")
     public void searchBooks() {
--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -1,11 +1,11 @@
-package Runner;
+package runner;
 
 import Execution.Tesbo;
 
 public class Runner {
     public static void main(String[] args) {
         Tesbo tests = new Tesbo();
-        tests.run();
+        tests.run(args);
     }
 }
 
