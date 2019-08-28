a918de9528211d032b73c22cd7d37535defc7019
Viral Patel <vir@qable.io>
2019-08-28 15:24:19 +0530

    Add files via upload

--- src/test/java/ExtTestCode/code.java
+++ src/test/java/ExtTestCode/code.java
@@ -6,18 +6,24 @@ import Selenium.ExtendTesboDriver;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-
+import reportAPI.Reporter;
 
 
 public class code extends ExtendTesboDriver{
     public code(WebDriver driver) {
         super(driver);
     }
+    Reporter reporter=new Reporter();
 
-    @ExtCode("Search books")
-    public void searchBooks() {
-        Assert.assertTrue(driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']")).isDisplayed());
-        driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']")).sendKeys("books");
+    @ExtCode("Verify profile page title")
+    public void VerifyProfilepageTitle() {
+        Assert.assertTrue(driver.findElement(By.xpath("//div[contains(text(),'Profile')]")).isDisplayed());
+        reporter.printStep("Profile page title is displayed!!!");
+    }
+
+    @ExtCode("Click on account button")
+    public void ClickOnAccountButton() {
+        driver.findElement(By.xpath("(//a[contains(text(),'Account')])[2]")).click();
     }
 
 }
