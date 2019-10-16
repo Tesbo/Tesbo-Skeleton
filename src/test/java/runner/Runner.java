c7c6d438f7ed6ece831d95da31e42e10db78163b
nishil <nishilpatel81@gmail.com>
2019-10-16 22:40:52 +0530

    Add TC for assertion select and close window

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -7,8 +7,6 @@ import java.nio.file.Paths;
 public class Runner {
     public static void main(String[] args) {
 
-        System.out.println(System.getProperty("user.dir"));
-        System.out.println(Paths.get("").toAbsolutePath().toString());
         Tesbo tests = new Tesbo();
         tests.run(args);
     }
